55af740734d3577065bdc51c823f389c2e16eb5a
Jeffbuchi <buchisam32@gmail.com>
2020-01-30 21:09:20 -0600

    creating a form action adding password to the form creating a passwor length of 8 to 16 characters long adding a confirm password to the form matching the password and confirm password

---
+++ logic.js
@@ -0,0 +1,26 @@
+document.getElementById("caption").innerHTML = "Venture Garden Registration Form Session 101";
+
+function InternsName (firstName, lastName) {
+    return `My fullname is ${lastName} ${firstName}`;
+}
+
+console.log(InternsName("Mormah", "Jefferson"))
+
+
+function validate() {
+    let password = document.getElementById("password").value;
+    let confirm_password = document.getElementById("confirm_password").value;
+
+    if (password.length < 8 || password.length > 16) {
+        alert("password length invalid");
+        return false;
+    }
+
+    if (password != confirm_password) {
+        alert("Password mismatch");
+        return false;
+    }
+    return true;
+}
+
+  
