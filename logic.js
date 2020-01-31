ba2430c5a02ec6f30de6f5531cf07f4e854a7851
Jeffbuchi <buchisam32@gmail.com>
2020-01-31 16:40:41 -0600

    adding further form fields validation

--- logic.js
+++ logic.js
@@ -1,26 +1,52 @@
 document.getElementById("caption").innerHTML = "Venture Garden Registration Form";
 
-function InternsName (firstName, lastName) {
-    return `My fullname is ${lastName} ${firstName}`;
-}
-
-console.log(InternsName("Mormah", "Jefferson"))
-
-
 function validate() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let age = document.getElementById("age").value;
     let password = document.getElementById("password").value;
     let confirm_password = document.getElementById("confirm_password").value;
+    let dropDown = document.getElementById("dropdown").value;
+    let beginner = document.getElementById("beginner").value;
+    let intermediate = document.getElementById("intermediate").value;
+    let advanced = document.getElementById("advanced").value;
+    let expert = document.getElementById("expert").value;
+    let text = document.getElementById("text").value;
 
-    if (password.length < 8 || password.length > 16) {
+    if (name == null || name == "" && email == null || email == "" && age == null || age == "" 
+    && dropDown == null || dropDown == "" && beginner == null || beginner == "" && intermediate
+    == null || intermediate == "" && advanced == null || advanced == "" && expert == null || expert
+    == "" && text == null || text == "") {
+        alert ("input fields"); 
+        return false;
+    }
+    
+    else if (password.length < 8 || password.length > 16) {
         alert("password length invalid");
         return false;
     }
 
-    if (password != confirm_password) {
+    else if (password != confirm_password) {
         alert("Password mismatch");
         return false;
     }
+    else {
+        console.log(`${name}`);
+        console.log(`${email}`);
+        console.log(`${age}`);
+        console.log(`${password}`);
+        console.log(`${confirm_password}`);
+        console.log(`${dropDown}`);
+        console.log(`${beginner}`);
+        console.log(`${intermediate}`);
+        console.log(`${advanced}`);
+        console.log(`${expert}`);
+        console.log(`${text}`);
+        alert("You have successfully submitted");
+    }
     return true;
 }
+
+
 
   
